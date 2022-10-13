51c9dc5d445b19f9e293bfe5446ff636b0c2da43
V3L0C1T13S <turtletester20@gmail.com>
2022-10-13 10:27:18 -0400

    chore: Stub /applications

--- src/api/routes/applications/index.ts
+++ src/api/routes/applications/index.ts
@@ -1,8 +1,18 @@
-import { Application } from "express";
+import { APIApplication } from "discord.js";
+import { Application, Response } from "express";
 import { Resource } from "express-automatic-routes";
 
 export default (express: Application) => <Resource> {
-  get: (req, res) => {
-    res.sendStatus(204);
+  get: async (req, res) => {
+    res.status(500).json({
+      code: 500,
+      message: "FIXME! Route unimpl.",
+    });
+  },
+  post: async (req, res) => {
+    res.status(500).json({
+      code: 500,
+      message: "FIXME! Route unimpl.",
+    });
   },
 };
