87c6fb65d7b241f67d5c9115ed2f9ddeca967e70
V3L0C1T13S <turtletester20@gmail.com>
2022-10-14 12:03:54 -0400

    feat: Implement PresenceUpdate OPCode

--- src/gateway/opcodes/PresenceUpdate.ts
+++ src/gateway/opcodes/PresenceUpdate.ts
@@ -4,7 +4,7 @@ import { WebSocket } from "../Socket";
 export async function presenceUpdate(this: WebSocket, data: Payload) {
   // FIXME: Make this better/use quarkconversion
 
-  await this.rvClient.user?.update({
+  await this.rvClient.users.edit({
     status: {
       text: data.d.activities[0]?.name,
     },
