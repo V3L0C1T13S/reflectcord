d0a7c68241726b50faf5f262ab6680d23d2d079b
V3L0C1T13S <turtletester20@gmail.com>
2022-10-14 11:32:44 -0400

    feat: implement /channel/messages GET

--- src/api/routes/channels/{channel_id}/messages/index.ts
+++ src/api/routes/channels/{channel_id}/messages/index.ts
@@ -6,6 +6,20 @@ import { Message } from "../../../../../common/models";
 import { createAPI } from "../../../../../common/rvapi";
 
 export default (express: Application) => <Resource> {
+  get: async (req, res) => {
+    const { channel_id } = req.params;
+
+    const api = createAPI(req.token);
+
+    const msgs = await api.get(`/channels/${channel_id}/messages`)
+      .catch(() => {
+        res.sendStatus(500);
+      }) as any;
+    if (!msgs) return;
+
+    return res.json(await Promise.all(msgs.messages
+      .map((msg: any) => Message.from_quark(msg))));
+  },
   post: async (req, res) => {
     const { channel_id } = req.params;
     const { content } = JSON.parse(req.body.payload_json);
