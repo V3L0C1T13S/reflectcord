d1fa7f41484f8ad9268ea3a525e2664dc0f360b9
V3L0C1T13S <turtletester20@gmail.com>
2022-10-15 20:53:59 -0400

    chore: remove fixme

--- src/gateway/opcodes/PresenceUpdate.ts
+++ src/gateway/opcodes/PresenceUpdate.ts
@@ -3,8 +3,6 @@ import { WebSocket } from "../Socket";
 import { Status } from "../../common/models";
 
 export async function presenceUpdate(this: WebSocket, data: Payload) {
-  // FIXME: Make this better/use quarkconversion
-
   await this.rvAPI.patch("/users/@me", {
     status: await Status.to_quark(data.d) as any,
   });
