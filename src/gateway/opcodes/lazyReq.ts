e2961c139aa0d946ac21b81dbc6470f46de97f91
V3L0C1T13S <turtletester20@gmail.com>
2022-10-23 17:54:39 -0400

    chore: Remove old member lazy request

--- src/gateway/opcodes/lazyReq.ts
+++ src/gateway/opcodes/lazyReq.ts
@@ -7,7 +7,6 @@ import { Member } from "../../common/models";
 import { Send, Payload } from "../util";
 import { WebSocket } from "../Socket";
 import { fromSnowflake } from "../../common/models/util";
-import { Logger } from "../../common/utils";
 import { check } from "./instanceOf";
 import { LazyRequest } from "../../common/sparkle/schemas";
 import "missing-native-js-functions";
@@ -58,7 +57,7 @@ function partition<T>(array: T[], isValid: Function) {
   );
 }
 
-async function getMembersV2(this: WebSocket, guild_id: string, range: LazyOperatorRange) {
+async function getMembers(this: WebSocket, guild_id: string, range: LazyOperatorRange) {
   if (!Array.isArray(range) || range.length !== 2) throw new Error("invalid range");
 
   const groups: LazyGroup[] = [];
@@ -150,85 +149,6 @@ async function getMembersV2(this: WebSocket, guild_id: string, range: LazyOperat
   };
 }
 
-async function getMembers(this: WebSocket, guild_id: string, range: [number, number]) {
-  if (!Array.isArray(range) || range.length !== 2) throw new Error("invalid range");
-
-  const members = await this.rvAPI.get(`/servers/${guild_id}/members`, {
-    exclude_offline: true,
-  }) as API.AllMemberResponse;
-
-  let discordMember = await Promise.all(members.members
-    .map((m) => Member.from_quark(m)));
-
-  const groups: any[] = [];
-  const items: any[] = [];
-  const roles = discordMember
-    .map((m) => m.roles)
-    .flat()
-    .unique((r) => r);
-  roles.push(
-    roles.splice(
-      roles.findIndex((x) => x === guild_id),
-      1,
-    )[0]!,
-  );
-
-  for (const role of roles) {
-    // @ts-ignore
-    const [role_members, other_members]: [APIGuildMember[], APIGuildMember[]] = partition(
-      discordMember,
-      (m: APIGuildMember) => m.roles
-        .find((r) => r === role),
-    );
-    const group = {
-      count: role_members.length,
-      id: role === guild_id ? "online" : role,
-    };
-
-    items.push({ group });
-    groups.push(group);
-
-    for (const member of role_members) {
-      const memberRoles = member.roles.filter((x) => x !== guild_id).map((x) => x);
-
-      const statusPriority = {
-        online: 0,
-        idle: 1,
-        dnd: 2,
-        invisible: 3,
-        offline: 4,
-      };
-
-      const session = {
-        status: "online",
-      };
-
-      const item = {
-        member: {
-          ...member,
-          roles,
-          user: { ...member.user, sessions: undefined },
-          presence: {
-            ...session,
-            activities: [],
-            user: { id: member.user?.id },
-          },
-        },
-      };
-
-      items.push(item);
-    }
-    discordMember = other_members;
-  }
-
-  return {
-    items,
-    groups,
-    range,
-    members: items.map((x) => ("member" in x ? x.member : undefined)).filter((x) => !!x),
-  };
-}
-
 export async function lazyReq(this: WebSocket, data: Payload) {
   check.call(this, LazyRequest, data.d);
   const {
@@ -244,7 +164,7 @@ export async function lazyReq(this: WebSocket, data: Payload) {
   const ranges = channels![channel_id];
   if (!Array.isArray(ranges)) throw new Error("Not a valid Array");
 
-  const ops = await getMembersV2.call(this, rvServerId, [0, 99]);
+  const ops = await getMembers.call(this, rvServerId, [0, 99]);
   const member_count = ops.members.length;
 
   const { groups } = ops;
