fc6a33ce53a876e0be193d20ace6a556536eb793
V3L0C1T13S <turtletester20@gmail.com>
2022-10-24 11:49:05 -0400

    feat: Partially implement /guilds/emojis

--- src/api/routes/guilds/{guild_id}/emojis/index.ts
+++ src/api/routes/guilds/{guild_id}/emojis/index.ts
@@ -1,8 +1,19 @@
 /* eslint-disable camelcase */
 import { Resource } from "express-automatic-routes";
+import { PartialEmoji } from "common/models";
+import { HTTPError } from "../../../../../common/utils";
+import { fromSnowflake } from "../../../../../common/models/util";
 
+// FIXME
 export default () => <Resource> {
-  get: (req, res) => {
+  get: async (req, res) => {
     const { guild_id } = req.params;
+
+    if (!guild_id) throw new HTTPError("Invalid params");
+
+    const serverId = await fromSnowflake(guild_id);
+
+    const emojis = await res.rvAPI.get(`/servers/${serverId}/emojis`);
+    if (!Array.isArray(emojis)) throw new HTTPError("Failed to get emojis");
   },
 };
