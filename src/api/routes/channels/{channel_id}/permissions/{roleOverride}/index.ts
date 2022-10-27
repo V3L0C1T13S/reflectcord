5f4d35adf3231da9547f321014e5199b9c765587
V3L0C1T13S <turtletester20@gmail.com>
2022-10-27 09:06:17 -0400

    fix: Actually create role overrides

--- src/api/routes/channels/{channel_id}/permissions/{roleOverride}/index.ts
+++ src/api/routes/channels/{channel_id}/permissions/{roleOverride}/index.ts
@@ -16,16 +16,22 @@ export default () => <Resource> {
     const rvChannelId = await fromSnowflake(channel_id);
     const roleId = await fromSnowflake(roleOverride);
 
-    const channel = await res.rvAPI.get(`/channels/${rvChannelId}/`) as API.Channel;
-
-    if (!("server" in channel)) throw new HTTPError("Channel not found", 404);
-
     // FIXME: Member overrides are unsupported by Revolt
     if (req.body.type !== OverwriteType.Role) throw new HTTPError("Invalid type");
+
+    // FIXME: Partial
+    await res.rvAPI.put(`/channels/${rvChannelId}/permissions/${roleId}`, {});
 
     res.sendStatus(204);
   },
   delete: async (req, res) => {
+    const { channel_id, roleOverride } = req.params;
+
+    if (!roleOverride || !channel_id) throw new HTTPError("Invalid params");
+
+    const rvChannelId = await fromSnowflake(channel_id);
+    const roleId = await fromSnowflake(roleOverride);
+
     res.sendStatus(204);
   },
 };
