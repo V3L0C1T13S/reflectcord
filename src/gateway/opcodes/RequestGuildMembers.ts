8d3d501cb551ec0783f54cfccd14bf8b543ee42b
V3L0C1T13S <turtletester20@gmail.com>
2022-11-06 12:47:54 -0500

    feat(gateway): Improve op8 uid search

--- src/gateway/opcodes/RequestGuildMembers.ts
+++ src/gateway/opcodes/RequestGuildMembers.ts
@@ -2,42 +2,54 @@
 /* eslint-disable camelcase */
 import {
   GatewayOpcodes, GatewayDispatchEvents,
-  GatewayRequestGuildMembersDataWithUserIds, GatewayRequestGuildMembersData, APIGuildMember,
+  GatewayRequestGuildMembersDataWithUserIds,
+  GatewayRequestGuildMembersData,
+  APIGuildMember,
 } from "discord.js";
 import { Payload, Send } from "../util";
 import { fromSnowflake } from "../../common/models/util";
 import { WebSocket } from "../Socket";
 import { Member } from "../../common/models";
 import { gatewayEnableOp8 } from "../../common/constants";
-import { Logger } from "../../common/utils";
 
-const filterMember = (x: APIGuildMember, user_ids: string[]) => x.user?.id === user_ids
-  .find((u) => x.user?.id === u);
+const memberExists = (uid: string, member_ids: string[]) => member_ids.includes(uid);
 
-const memberExists = (uid: string, member_ids: string[]) => member_ids.find((x) => x === uid);
+const isUidSearch = (data: GatewayRequestGuildMembersData): data is GatewayRequestGuildMembersDataWithUserIds => ("user_ids" in data);
+
+type RequestGuildMembersBody = {
+  guild_id: string,
+  members: APIGuildMember[],
+  not_found?: string[],
+  presences?: any[],
+}
 
 export async function RequestGuildMembers(
   this: WebSocket,
-  data: Payload<GatewayRequestGuildMembersDataWithUserIds>,
+  data: Payload<GatewayRequestGuildMembersData>,
 ) {
   if (!gatewayEnableOp8) return;
 
   if (!data.d) return;
 
   const {
-    guild_id, presences, nonce, user_ids,
+    guild_id, presences, nonce,
   } = data.d;
 
-  const realGuildId = Array.isArray(guild_id) ? (guild_id?.[0]) : guild_id;
+  const body: RequestGuildMembersBody = {
+    guild_id,
+    members: [],
+  };
+
+  const realGuildId = Array.isArray(guild_id) ? (guild_id?.[0] as string) : guild_id;
 
   const rvId = await fromSnowflake(realGuildId);
 
   const limit = 1000;
 
   // FIXME: We probably shouldn't get every single member, or at least cache this for later.
-  const members = (await this.rvAPI.get(`/servers/${rvId as ""}/members`, {
+  const members = await this.rvAPI.get(`/servers/${rvId as ""}/members`, {
     exclude_offline: true,
-  }));
+  });
 
   members.members.splice(limit);
   members.users.splice(limit);
@@ -45,21 +57,27 @@ export async function RequestGuildMembers(
   const discordMembers = (await Promise.all(members.members
     .map((x) => Member.from_quark(x, members.users.find((u) => u._id === x._id.user)))));
 
-  const notFound: string[] = [];
+  body.members = discordMembers;
 
-  if (Array.isArray(user_ids)) {
-    notFound.push(...(user_ids
-      .filter((x) => memberExists(x, discordMembers.map((m) => m.user?.id ?? "0")))));
+  if (isUidSearch(data.d)) {
+    const { user_ids } = data.d;
+
+    const notFound: string[] = [];
+
+    if (Array.isArray(user_ids)) {
+      notFound.push(...(user_ids
+        .filter((x) => !memberExists(x, discordMembers.map((m) => m.user?.id ?? "0")))));
+    }
+
+    body.members = discordMembers.filter((x) => (x.user ? user_ids.includes(x.user.id) : false));
+
+    body.not_found = notFound;
   }
 
   await Send(this, {
     op: GatewayOpcodes.Dispatch,
     t: GatewayDispatchEvents.GuildMembersChunk,
     s: this.sequence++,
-    d: {
-      guild_id,
-      members: discordMembers,
-      not_found: notFound,
-    },
+    d: body,
   });
 }
