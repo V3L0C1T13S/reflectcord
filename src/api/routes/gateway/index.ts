bfe3c803ae5e5080801479ea9c6f863cd2e50d9d
V3L0C1T13S <turtletester20@gmail.com>
2022-11-07 16:49:43 -0500

    fix(api): Use WS url constant here too

--- src/api/routes/gateway/index.ts
+++ src/api/routes/gateway/index.ts
@@ -1,10 +1,11 @@
 import { Application } from "express";
 import { Resource } from "express-automatic-routes";
+import { reflectcordWsURL } from "../../../common/constants";
 
 export default (express: Application) => <Resource> {
   get: (req, res) => {
     res.json({
-      url: "ws://localhost:3002",
+      url: reflectcordWsURL,
     });
   },
 };
