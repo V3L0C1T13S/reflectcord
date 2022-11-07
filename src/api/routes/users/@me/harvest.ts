7a92e70baa663a899015cc255f9871fe73990222
V3L0C1T13S <turtletester20@gmail.com>
2022-11-07 13:59:31 -0500

    feat(api): Stub /users/me/harvest

--- src/api/routes/users/@me/harvest.ts
+++ src/api/routes/users/@me/harvest.ts
@@ -2,7 +2,7 @@ import { Resource } from "express-automatic-routes";
 
 // STUB
 export default () => <Resource> {
-  post: (req, res) => {
+  get: (req, res) => {
     res.sendStatus(204);
   },
 };
