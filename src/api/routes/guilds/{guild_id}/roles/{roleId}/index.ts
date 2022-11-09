45baa3349f141650595a32f985d426a2f7176177
V3L0C1T13S <turtletester20@gmail.com>
2022-11-09 12:26:56 -0500

    feat(api): Implement role DELETE

--- src/api/routes/guilds/{guild_id}/roles/{roleId}/index.ts
+++ src/api/routes/guilds/{guild_id}/roles/{roleId}/index.ts
@@ -1,5 +1,25 @@
+/* eslint-disable camelcase */
 import { Resource } from "express-automatic-routes";
+import { fromSnowflake } from "../../../../../../common/models/util";
+import { HTTPError } from "../../../../../../common/utils";
 
 export default () => <Resource> {
+  // FIXME
   get: (req, res) => {},
+  // FIXME
+  patch: (req, res) => {
+    res.sendStatus(500);
+  },
+  delete: async (req, res) => {
+    const { guild_id, roleId } = req.params;
+
+    if (!roleId || !guild_id) throw new HTTPError("Invalid params");
+
+    const serverId = await fromSnowflake(guild_id);
+    const rvRoleId = await fromSnowflake(roleId);
+
+    await res.rvAPI.delete(`/servers/${serverId as ""}/roles/${rvRoleId as ""}`);
+
+    res.sendStatus(204);
+  },
 };
