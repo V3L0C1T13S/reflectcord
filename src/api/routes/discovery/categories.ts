c8e0955775cb6c1d1bfd199615a73bb8023aad27
V3L0C1T13S <turtletester20@gmail.com>
2022-11-17 13:22:24 -0500

    feat(api): Make discovery categories captialized

--- src/api/routes/discovery/categories.ts
+++ src/api/routes/discovery/categories.ts
@@ -1,11 +1,12 @@
-import { Application, Response } from "express";
+import { Response } from "express";
 import { Resource } from "express-automatic-routes";
 import axios from "axios";
+import { startCase } from "lodash";
 import { ServerDiscoveryResponse } from "../../../common/rvapi";
 import { getRevoltDiscoveryDataURL } from "../../../common/constants";
 import { DiscoveryCategory } from "../../../common/sparkle";
 
-export default (express: Application) => <Resource> {
+export default () => <Resource> {
   get: async (req, res: Response<DiscoveryCategory[]>) => {
     const discoveryURL = await getRevoltDiscoveryDataURL();
     const revoltServers = await axios.get<ServerDiscoveryResponse>(`${discoveryURL}/discover/servers.json`);
@@ -13,7 +14,7 @@ export default (express: Application) => <Resource> {
     res.json(revoltServers.data.pageProps.popularTags.map((x, i) => ({
       id: i,
       is_primary: false,
-      name: x,
+      name: startCase(x),
     })));
   },
 };
