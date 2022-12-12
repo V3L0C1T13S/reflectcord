bf66c5638b17e27fa1450636d267200163329de1
V3L0C1T13S <turtletester20@gmail.com>
2022-12-12 14:02:14 -0500

    feat: Partially implement locale translation

--- src/common/models/models/settings.ts
+++ src/common/models/models/settings.ts
@@ -13,25 +13,36 @@ export interface RevoltThemeSetting {
 
 export interface RevoltSettings {
   appearance: RevoltSetting,
-  theme: RevoltSetting
+  theme: RevoltSetting,
+  locale: RevoltSetting,
 }
 
-export const SettingsKeys = ["appearance", "theme"];
+export const SettingsKeys = ["appearance", "theme", "locale"];
+
+const LocaleMap: Record<string, string> = {
+  de: "de",
+  en_US: "en-US",
+  en: "en-GB",
+  es: "es-ES",
+};
 
 export const UserSettings: QuarkConversion<RevoltSettings, DiscordUserSettings> = {
   async to_quark(settings) {
     return {
       appearance: [Date.now(), ""],
       theme: [Date.now(), ""],
+      locale: [Date.now(), JSON.stringify(settings.locale)],
     };
   },
 
   async from_quark(settings) {
     const themeSettings: RevoltThemeSetting = JSON.parse(settings.theme[1]);
+    const localeSettings = JSON.parse(settings.locale[1]);
 
     return {
       ...DefaultUserSettings,
       theme: themeSettings["appearance:theme:base"] === "light" ? "light" : "dark",
+      locale: LocaleMap[localeSettings["lang"]] ?? "en-US",
     };
   },
 };
@@ -57,6 +68,14 @@ export async function settingsToProtoBuf(settings: DiscordUserSettings) {
       guild_onboarding_progress: 0,
     },
     user_content: {},
+    localization: {
+      locale: {
+        locale_code: settings.locale,
+        timezone_offset: {
+          offset: settings.timezone_offset,
+        },
+      },
+    },
     appearance: {
       theme: settings.theme === "light" ? 2 : 1,
       developer_mode: settings.developer_mode,
@@ -68,6 +87,11 @@ export async function settingsToProtoBuf(settings: DiscordUserSettings) {
       custom_status: {},
       show_current_game: {
         value: true,
+      },
+    },
+    voice_and_video: {
+      blur: {
+        use_blur: false,
       },
     },
   }).finish();
