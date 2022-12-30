4536a47d2e2d707c9b4d4f6ae6ac9c222b3a39dd
V3L0C1T13S <turtletester20@gmail.com>
2022-12-29 23:22:52 -0500

    feat(api): Implement channel role overrides

--- src/api/routes/channels/{channel_id}/permissions/{roleOverride}/index.ts
+++ src/api/routes/channels/{channel_id}/permissions/{roleOverride}/index.ts
@@ -1,7 +1,7 @@
 /* eslint-disable camelcase */
 import { OverwriteType } from "discord.js";
 import { Resource } from "express-automatic-routes";
-import { fromSnowflake } from "@reflectcord/common/models";
+import { fromSnowflake, Permissions } from "@reflectcord/common/models";
 import { HTTPError } from "@reflectcord/common/utils";
 
 // FIXME
@@ -12,13 +12,29 @@ export default () => <Resource> {
     if (!roleOverride || !channel_id) throw new HTTPError("Invalid params");
 
     const rvChannelId = await fromSnowflake(channel_id);
-    const roleId = await fromSnowflake(roleOverride);
+    let roleId = await fromSnowflake(roleOverride);
 
     // FIXME: Member overrides are unsupported by Revolt
     if (req.body.type !== OverwriteType.Role) throw new HTTPError("Invalid type");
 
-    // FIXME: Partial
-    await res.rvAPI.put(`/channels/${rvChannelId}/permissions/${roleId}`, {});
+    const rvChannel = await res.rvAPIWrapper.channels.fetch(rvChannelId);
+    if (!("guild_id" in rvChannel.discord)) throw new HTTPError("Invalid channel", 400);
+    if (rvChannel.discord.guild_id === roleOverride) {
+      roleId = "default";
+    }
+
+    const allow = BigInt(req.body.allow);
+    const deny = BigInt(req.body.deny);
+
+    const rvAllow = await Permissions.to_quark(allow);
+    const rvDeny = await Permissions.to_quark(deny);
+
+    await res.rvAPI.put(`/channels/${rvChannelId as ""}/permissions/${roleId as ""}`, {
+      permissions: {
+        allow: rvAllow.a,
+        deny: rvDeny.a,
+      },
+    });
 
     res.sendStatus(204);
   },
