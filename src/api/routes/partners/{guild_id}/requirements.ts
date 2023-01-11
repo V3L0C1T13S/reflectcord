215c31fa9e4244412d24e9b43fd5aae1b632ba5d
V3L0C1T13S <turtletester20@gmail.com>
2023-01-11 14:29:51 -0500

    feat(api): Partner program stubs chore(deps): Update packages

--- src/api/routes/partners/{guild_id}/requirements.ts
+++ src/api/routes/partners/{guild_id}/requirements.ts
@@ -1,11 +1,10 @@
 /* eslint-disable camelcase */
-import { Response } from "express";
 import { Resource } from "express-automatic-routes";
 import { HTTPError } from "@reflectcord/common/utils";
-import { DiscoveryRequirements, stubDiscoveryRequirements } from "@reflectcord/common/sparkle";
+import { stubDiscoveryRequirements } from "@reflectcord/common/sparkle";
 
 export default () => <Resource> {
-  get: (req, res: Response<DiscoveryRequirements>) => {
+  get: (req, res) => {
     const { guild_id } = req.params;
 
     if (!guild_id) throw new HTTPError("Invalid params");
--- src/common/sparkle/schemas/Discovery/requirements.ts
+++ src/common/sparkle/schemas/Discovery/requirements.ts
@@ -25,3 +25,31 @@ export interface DiscoveryRequirements {
   health_score: DiscoveryHealth_score;
   minimum_size: number;
 }
+
+export const stubDiscoveryRequirements = {
+  guild_id: "0",
+  safe_environment: true,
+  healthy: true,
+  health_score_pending: false,
+  size: true,
+  nsfw_properties: {},
+  protected: true,
+  sufficient: true,
+  sufficient_without_grace_period: true,
+  valid_rules_channel: true,
+  retention_healthy: true,
+  engagement_healthy: true,
+  age: true,
+  minimum_age: 56,
+  health_score: {
+    avg_nonnew_participators: 999999,
+    avg_nonnew_communicators: 999999,
+    num_intentful_joiners: 999999,
+    perc_ret_w1_intentful: 999999,
+  },
+  /**
+   * rvlt.gg accepts servers of all sizes. May want to allow this to be adjusted
+   * for open discovery
+  */
+  minimum_size: 0,
+};
