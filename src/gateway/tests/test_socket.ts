85761f3ac12dddbf0154c22af2a15aed09e58e16
V3L0C1T13S <turtletester20@gmail.com>
2023-01-12 10:27:26 -0500

    tests: Make gateway test work

--- src/gateway/tests/test_socket.ts
+++ src/gateway/tests/test_socket.ts
@@ -1,4 +1,7 @@
+import { GatewayDispatchEvents, GatewayOpcodes } from "discord.js";
 import { WebSocket } from "ws";
+import { TestingToken } from "@reflectcord/common/rvapi";
+import { GatewayDispatchCodes } from "@reflectcord/common/sparkle/schemas/Gateway/Dispatch";
 
 const socket = new WebSocket("ws://localhost:3002?v=9&encoding=json");
 
@@ -7,13 +10,34 @@ socket.onopen = () => {
 };
 
 socket.onmessage = (data) => {
-  console.log("got data", data);
+  console.log("got data", data.data);
 
-  const d = data.data as unknown as { op: number };
+  const d = JSON.parse(data.data as string) as unknown as { op: number, t?: string };
 
   switch (d.op) {
     case 10: {
       console.log("gw is awaiting auth");
+      socket.send(Buffer.from(JSON.stringify({
+        op: GatewayOpcodes.Identify,
+        d: { token: TestingToken },
+      })));
+      break;
+    }
+    case GatewayOpcodes.Dispatch: {
+      switch (d.t!) {
+        case GatewayDispatchEvents.Ready: {
+          console.log("GW Authenticated.");
+          break;
+        }
+        case GatewayDispatchCodes.ReadySupplemental: {
+          console.log("Supplemental received");
+          break;
+        }
+        default: {
+          console.log(d.t);
+          break;
+        }
+      }
       break;
     }
     default:
