ef008028f5bd8021481085ecbae882dc6abe94a3
V3L0C1T13S <turtletester20@gmail.com>
2023-04-03 08:00:05 -0400

    fix: Change channel mention regex to global search

--- src/common/utils/discord/revolt_regex.ts
+++ src/common/utils/discord/revolt_regex.ts
@@ -1,4 +1,4 @@
-export const REVOLT_CHANNEL_MENTION = /<#(.*)>/gs;
+export const REVOLT_CHANNEL_MENTION = /<#(.*)>/g;
 export const REVOLT_USER_MENTION = /<@!?(.*)>/g;
 export const REVOLT_EMOJI_REGEX = /:([a-zA-Z0-9_+]+):/g;
 export const REVOLT_ULID = /^[0123456789ABCDEFGHJKMNPQRSTVWXYZ]{26}$/;
