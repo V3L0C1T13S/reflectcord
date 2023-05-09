c76e0410a6c8fb4f9ad8fcaf0beee3c68992123b
V3L0C1T13S <turtletester20@gmail.com>
2023-05-09 11:31:19 -0400

    fix: proper channel and users regex

--- src/common/utils/discord/revolt_regex.ts
+++ src/common/utils/discord/revolt_regex.ts
@@ -1,5 +1,5 @@
-export const REVOLT_CHANNEL_MENTION = /<#(.*)>/g;
-export const REVOLT_USER_MENTION = /<@!?(.*)>/g;
+export const REVOLT_CHANNEL_MENTION = /<#([0-9ABCDEFGHJKMNPQRSTVWXYZ]{26})>/g;
+export const REVOLT_USER_MENTION = /<@([0-9ABCDEFGHJKMNPQRSTVWXYZ]{26})>/g;
 export const REVOLT_EMOJI_REGEX = /:([a-zA-Z0-9_+]+):/g;
 export const REVOLT_ULID = /^[0123456789ABCDEFGHJKMNPQRSTVWXYZ]{26}$/;
 
