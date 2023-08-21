b2936577fb47eb7e1965c2f50eff07da4b2d185b
V3L0C1T13S <turtletester20@gmail.com>
2023-08-21 13:32:23 -0400

    fix(api): Change migrate-command-scope to POST

--- src/api/routes/guilds/{guild_id}/migrate-command-scope.ts
+++ src/api/routes/guilds/{guild_id}/migrate-command-scope.ts
@@ -1,7 +1,7 @@
 import { Resource } from "express-automatic-routes";
 
 export default () => <Resource> {
-  get: (req, res) => {
+  post: (req, res) => {
     res.json({
       integration_ids_with_app_commands: [],
     });
