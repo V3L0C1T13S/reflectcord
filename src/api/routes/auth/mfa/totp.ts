060f66bd53e200b338380090866c8aaf9a4d64d0
V3L0C1T13S <turtletester20@gmail.com>
2023-09-07 12:34:10 -0400

    fix(api): create session for TOTP login

--- src/api/routes/auth/mfa/totp.ts
+++ src/api/routes/auth/mfa/totp.ts
@@ -1,9 +1,20 @@
 import { Resource } from "express-automatic-routes";
 import { DataMFALogin, ResponseLogin } from "@reflectcord/common/models";
+import { RevoltSession } from "@reflectcord/common/mongoose";
 
 export default () => <Resource> {
   post: async (req, res) => {
     const loginResponse = await res.rvAPI.post("/auth/session/login", await DataMFALogin.to_quark(req.body));
+
+    if (loginResponse.result === "Success") {
+      await RevoltSession.create({
+        _id: loginResponse._id,
+        token: loginResponse.token,
+        user_id: loginResponse.user_id,
+        name: loginResponse.name,
+        result: loginResponse.result,
+      });
+    }
 
     res.json(await ResponseLogin.from_quark(loginResponse));
   },
