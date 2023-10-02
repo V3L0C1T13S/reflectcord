acdb5b800b9e5f3dca982f9e026b3f14aaf28556
V3L0C1T13S <turtletester20@gmail.com>
2023-10-02 16:17:06 -0400

    feat(sparkle): RemoteCommand typings fix(gateway): Fix RemoteCommand implementation

--- src/common/sparkle/schemas/Gateway/RemoteCommand.ts
+++ src/common/sparkle/schemas/Gateway/RemoteCommand.ts
@@ -1,4 +1,33 @@
-export interface RemoteCommandSchema {
-  target_session_id: string,
-  payload: any,
+export enum RemoteCommandPayloadType {
+  VoiceStateUpdate = "VOICE_STATE_UPDATE",
+  Disconnect = "DISCONNECT"
 }
+
+export interface RemoteCommandBasePayload {
+  type: RemoteCommandPayloadType
+}
+
+export interface RemoteCommandVoiceStateUpdatePayload extends RemoteCommandBasePayload {
+  self_mute: boolean,
+  self_deaf: boolean,
+  type: RemoteCommandPayloadType.VoiceStateUpdate,
+}
+
+export interface RemoteCommandDisconnectPayload extends RemoteCommandBasePayload {
+  type: RemoteCommandPayloadType.Disconnect
+}
+
+export interface BaseRemoteCommand {
+  target_session_id: string,
+  payload: RemoteCommandBasePayload,
+}
+
+export interface RemoteCommandVoiceStateUpdate extends BaseRemoteCommand {
+  payload: RemoteCommandVoiceStateUpdatePayload,
+}
+
+export interface RemoteCommandDisconnect extends BaseRemoteCommand {
+  payload: RemoteCommandDisconnectPayload,
+}
+
+export type RemoteCommandSchema = RemoteCommandVoiceStateUpdate | RemoteCommandDisconnect;
--- src/gateway/opcodes/RemoteCommand.ts
+++ src/gateway/opcodes/RemoteCommand.ts
@@ -1,9 +1,9 @@
 /* eslint-disable camelcase */
 import { RemoteCommandSchema } from "@reflectcord/common/sparkle";
+import { Logger } from "@reflectcord/common/utils";
 import { SessionManager } from "../managers";
 import { WebSocket } from "../Socket";
 import { Payload } from "../util";
-import { invokeOPCode } from "../events";
 
 export async function RemoteCommand(this: WebSocket, data: Payload<RemoteCommandSchema>) {
   const { target_session_id, payload } = data.d!;
@@ -11,5 +11,11 @@ export async function RemoteCommand(this: WebSocket, data: Payload<RemoteCommand
   const session = SessionManager.getSession(target_session_id);
   if (!session || session.rv_user_id !== this.rv_user_id) return;
 
-  await invokeOPCode.call(session, payload);
+  switch (payload.type) {
+    default: {
+      Logger.debug("Unhandled remote command:", payload.type);
+
+      break;
+    }
+  }
 }
