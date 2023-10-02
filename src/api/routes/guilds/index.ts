d75408207579873f7f1394cfdd7b9bc8ce3cf23f
V3L0C1T13S <turtletester20@gmail.com>
2023-10-02 16:20:20 -0400

    feat(api): Use createServer rvapi abstraction

--- src/api/routes/guilds/index.ts
+++ src/api/routes/guilds/index.ts
@@ -1,18 +1,21 @@
-import { Application } from "express";
 import { Resource } from "express-automatic-routes";
 import { HTTPError } from "@reflectcord/common/utils";
-import { Guild } from "@reflectcord/common/models";
+import { ChannelCreateBody } from "@reflectcord/common/models";
+import { Request } from "express";
+import { RESTPostAPIGuildsJSONBody } from "discord.js";
 
 export default () => <Resource> {
-  post: async (req, res) => {
-    const { name } = req.body;
+  post: async (req: Request<{}, {}, RESTPostAPIGuildsJSONBody>, res) => {
+    const { name, channels } = req.body;
 
     if (!name) throw new HTTPError("Guild must be named", 422);
 
-    const server = await res.rvAPI.post("/servers/create", {
+    const server = await res.rvAPIWrapper.servers.createServer({
       name,
+      channels: channels ? await Promise.all(channels
+        .map((x) => ChannelCreateBody.to_quark(x))) : null,
     });
 
-    res.json(await Guild.from_quark(server.server));
+    res.json(server.discord);
   },
 };
